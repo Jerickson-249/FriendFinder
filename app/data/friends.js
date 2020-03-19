8c07fd22dcf174b553d8dcb110cf6e53e8d91179
Jacob Erickson <jakedasnake6_12@hotmail.com>
2020-03-18 20:19:05 -0600

    Initial commit to upload the files I am working on

---
+++ app/data/friends.js
@@ -0,0 +1,31 @@
+var friends = [
+{
+    "name":"Matthew",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[5,1,4,4,5,1,2,5,4,1]
+  },
+  {
+    "name":"Mark",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[5,1,1,1,1,1,2,1,4,1]
+  },
+  {
+    "name":"Luke",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[5,3,2,1,1,5,2,3,4,2]
+  },
+  {
+    "name":"John",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[5,1,3,1,5,1,2,1,4,5]
+  },
+  {
+    "name":"Paul",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[3,2,4,1,2,4,2,1,3,2]
+  },
+];
+
+module.exports = friends;
+
+//***NEED DIFFERENT PHOTOS LINKS***
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,56 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var friends = require("../data/friends");
+console.log(friends)
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/friends", function(req, res) {
+      console.log("POST: ", req.body)
+    
+    /// build the matching 
+    // you will get the object in the req.body with name , pic and array of responses
+    // you will compare the responses with the responses of all the friends inside of the array 
+    // you will return the name and pic of the friend with the minimun diference 
+    res.json(["Jacob","photo"])
+});
+
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+
+
+  app.post("/api/clear", function(req, res) {
+    // Empty out the arrays of data
+    friends.length = 0;
+
+    res.json({ ok: true });
+  });
+};
