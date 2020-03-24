9259c29e304ea559537d40fa8e7aa7b74e66be77
Jacob Erickson <jakedasnake6_12@hotmail.com>
2020-03-23 19:54:30 -0600

    Updated files for more functionality

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,14 +12,14 @@ console.log(friends)
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/friends", function(req, res) {
+  app.get("/api/friends", function (req, res) {
     res.json(friends);
   });
 
@@ -31,15 +31,39 @@ module.exports = function(app) {
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/friends", function(req, res) {
-      console.log("POST: ", req.body)
-    
+  app.post("/api/friends", function (req, res) {
+    console.log("POST: ", req.body)
+    var newScores = req.body.scores
+    newScores = [1,2,3,4,5,1,2,3,4,5]
+    var bestMatch = {
+      name: "",
+      photo: ""
+    }
     /// build the matching 
     // you will get the object in the req.body with name , pic and array of responses
     // you will compare the responses with the responses of all the friends inside of the array 
     // you will return the name and pic of the friend with the minimun diference 
-    res.json(["Jacob","photo"])
-});
+    var minDiff = 100
+    for (var i = 0; i < friends.length; i++) { //loop the array of friends
+      var scores = friends[i].scores
+      var dif = 0
+      for (var j = 0; j < scores.length; j++) {  //   4-5 = 1  5-4 =1  // loop the array of scores
+
+        dif = dif + (Math.abs(scores[j] - newScores[j]))
+
+      }
+
+      if (dif < minDiff) {
+        bestMatch.name = friends[i].name
+        bestMatch.photo = friends[i].photo
+        minDiff = dif
+      }
+
+    }
+    console.log(bestMatch)
+    res.json("hi")
+
+  });
 
   // ---------------------------------------------------------------------------
   // I added this below code so you could clear out the table while working with the functionality.
@@ -47,7 +71,7 @@ module.exports = function(app) {
 
 
 
-  app.post("/api/clear", function(req, res) {
+  app.post("/api/clear", function (req, res) {
     // Empty out the arrays of data
     friends.length = 0;
 
