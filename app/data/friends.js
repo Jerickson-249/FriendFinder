ce33a04609ba709ca865a2deeea8365baa3b77bd
Jacob Erickson <jakedasnake6_12@hotmail.com>
2020-03-28 10:24:35 -0600

    updated photos

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friends = [
 {
     "name":"Matthew",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo":"https://www.learnreligions.com/thmb/yE3pO1h6PSdGvaGF-TuGGYfGD8s=/1500x844/smart/filters:no_upscale()/Matthew-GettyImages-112186109-5787c6175f9b5831b50e48ac.jpg",
     "scores":[5,1,4,4,5,1,2,5,4,1]
   },
   {
     "name":"Mark",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo":"https://www.learnreligions.com/thmb/2cFYID7g-uxNH9A0l00HXDgyIII=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/JohnMark-284b581de3a342cd8395fc397b3a517b.jpg",
     "scores":[5,1,1,1,1,1,2,1,4,1]
   },
   {
     "name":"Luke",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo":"https://www.learnreligions.com/thmb/d_1ZGI5JWeWwfHOWzCQcCPfbw70=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GospelofLuke-463920463-1ae18e394b0b44a3a53edf4fba44b75a.jpg",
     "scores":[5,3,2,1,1,5,2,3,4,2]
   },
   {
     "name":"John",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo":"https://www.learnreligions.com/thmb/R7P8v9n-kgpuSo68tuitDQjdmhQ=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Peter-and-John1500x1000-56a149243df78cf77269300a.jpg",
     "scores":[5,1,3,1,5,1,2,1,4,5]
   },
   {
     "name":"Paul",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo":"https://www.learnreligions.com/thmb/u7uMg87tzpa2Dszv46bPe3NW1us=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Apostle-Paul-58ebd8dd5f9b58ef7e7a2064.jpg",
     "scores":[3,2,4,1,2,4,2,1,3,2]
   },
 ];
